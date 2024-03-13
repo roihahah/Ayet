8d151c064b57aa1a859fe47a67b0a66a361bcab8
roihahah <71399762+roihahah@users.noreply.github.com>
2024-03-13 22:02:15 +0200

    Update users.js
    deleted some code that the server doesn't needs

--- users.js
+++ users.js
@@ -2,7 +2,7 @@ const User = require('./models/user')
 // Function to update user balance
 async function updateUserBalance(userId, amount) {
     try {
-        amount = parseInt(amount, 10); // Ensure amount is a number
+        amount = parseInt(amount, 10); 
         const user = await User.findOneAndUpdate(
             { userId: userId },
             { $inc: { amount: amount } }, // Increment user's amount
@@ -13,35 +13,12 @@ async function updateUserBalance(userId, amount) {
         console.error('Error updating user balance:', error);
     }
 }
-//take money from the user 
-// async function getMoneyFromUser(userId) {
-//     try {
-//         // First, find the user to get the current balance
-//         const user = await User.findOne({ userId: userId });
-//         if (!user) {
-//             console.log('User not found');
-//             return 0; // User not found, return 0 balance
-//         }
 
-//         const currentBalance = user.amount; // Store the current balance
-//         console.log(currentBalance)
-//         // Now, update the user's balance to 0
-//         await User.updateOne(
-//             { userId: userId },
-//             { $set: { amount: 0 } } // Reset amount to 0
-//         );
-
-//         return currentBalance; // Return the balance before resetting
-//     } catch (error) {
-//         console.error('Error getting money from user:', error);
-//         return 0;
-//     }
-// }
 
 
 // Export the functions for use in other files
 module.exports = {
     updateUserBalance,
-    getUserBalance,
+    
     
 };
